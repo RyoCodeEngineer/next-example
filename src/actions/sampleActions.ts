8132da622d94835cd352fefec938cb444b6498f4
RyoCode <flora.fluorite.caf2@gmail.com>
2024-05-29 14:12:32 +0900

    17. Server Actions Part2

---
+++ src/actions/sampleActions.ts
@@ -0,0 +1,9 @@
+"use server"; // Server Actionsに必須
+
+export const createTask = async (taskId: number, formData: FormData) => {
+  // DBにタスクを作成
+
+  console.log("タスクを作成しました");
+  console.log(formData.get("name"));
+  console.log(taskId);
+};
