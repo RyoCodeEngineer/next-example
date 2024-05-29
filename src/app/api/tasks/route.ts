70efe3e31fbc132c7f7f643287e01c0bab989724
RyoCode <flora.fluorite.caf2@gmail.com>
2024-05-29 13:31:40 +0900

    13. ルートハンドラー Part1

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+export interface Tasks {
+  id: number;
+  name: string;
+}
+
+const tasks: Tasks[] = [
+  { id: 1, name: "プログラミング" },
+  { id: 2, name: "ランニング" },
+];
+
+export const GET = async () => {
+  return NextResponse.json(
+    { tasks },
+    {
+      status: 200,
+    }
+  );
+};
+
+/**
+ * 返却値が毎回異なるような場合は
+ * リクエスト毎に動的に実行されるように設定する
+ * 「ルートセグメントコンフィグ」
+ */
+export const dynamic = "force-dynamic";
